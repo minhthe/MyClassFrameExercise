eccbcef66f8aab3bacc50f5a7bb312d910acde41
minhthe <minhthe.007@gmail.com>
2016-08-12 15:52:11 +0700

    Consavation Fagnment to Activity

--- app/src/main/java/com/example/ttran230/myclassframe/MainActivity.java
+++ app/src/main/java/com/example/ttran230/myclassframe/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements MyFragment.ConversationFA{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,19 @@ public class MainActivity extends FragmentActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        fragmentTransaction.add(R.id.ln1,new MyFragment());
-        fragmentTransaction.add(R.id.ln2,new MyFragment2());
+        fragmentTransaction.add(R.id.ln1,new MyFragment());
+//        fragmentTransaction.add(R.id.ln2,new MyFragment2());
         fragmentTransaction.commit();
 
+
+
+
+    }
+
+    @Override
+    public void sendMessage(String text) {
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("change: "+text);
     }
 }
--- app/src/main/java/com/example/ttran230/myclassframe/MyFragment.java
+++ app/src/main/java/com/example/ttran230/myclassframe/MyFragment.java
@@ -1,23 +1,44 @@
 package com.example.ttran230.myclassframe;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by ttran230 on 8/12/2016.
  */
 public class MyFragment extends Fragment {
 
+    Button btn;
+    ConversationFA cv;
+    public interface ConversationFA{
+        public void sendMessage(String text);
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.myfragment,container,false);
+        cv = (ConversationFA) getActivity();
+        btn = (Button) view.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv.sendMessage("from the fragment1");
+            }
+        });
         return view;
     }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        cv = (ConversationFA) getActivity();
+//    }
 }
